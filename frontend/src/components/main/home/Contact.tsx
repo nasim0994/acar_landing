ba1e084d5580b8feaa8ea63e67480ab429dfe0fe
nasim0994 <nasimuddin0994@gmail.com>
2025-03-12 16:11:32 +0600

    home page completed

--- frontend/src/components/main/home/Contact.tsx
+++ frontend/src/components/main/home/Contact.tsx
@@ -1,10 +1,18 @@
 export default function Contact() {
   return (
-    <section className="py-10 bg-gradient-to-t from-base-100 via-secondary/90 to-secondary">
+    <section className="pt-10 pb-20 bg-gradient-to-t from-base-100 via-secondary/90 to-secondary relative">
       <div className="container">
         <h2 className="text-center font-semibold text-neutral text-lg">
           প্রয়োজন হলে কল করুন: 01706260994, 01706260995
         </h2>
+      </div>
+
+      <div>
+        <img
+          src="/images/wave.png"
+          alt="wave"
+          className="w-full h-20 absolute -bottom-3 left-0"
+        />
       </div>
     </section>
   );
